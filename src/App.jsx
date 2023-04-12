35cd372aecca3ac0da005a19b9c9e06363564e53
Mohanraj <Mohanvijay8344@gmail.com>
2023-04-12 07:24:46 +0530

    Day-42-Task

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { SignUp } from "./SignUp";
+import { SignIn } from "./SignIn";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Home } from "./Home";
+import { Forgot } from "./Forgot";
+import { Reset } from "./Reset";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function App() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <AppBar className="App" sx={{ backgroundColor: "green" }}>
+          <Toolbar>
+            {/* <Button onClick={()=>navigate("/")} color="inherit">SIGNIN</Button> */}
+            <Button onClick={()=>navigate("/signup")} color="inherit">SIGNUP</Button>
+            <Button onClick={()=>navigate("/forgot-password")} color="inherit">FORGOT</Button>
+            <Button sx={{ marginLeft: "auto" }} onClick={()=>navigate("/")} color="inherit">SIGNOUT</Button>
+          </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/forgot-password" element={<Forgot />} />
+        <Route path="/reset-password" element={<Reset />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
